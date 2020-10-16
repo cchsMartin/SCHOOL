c115cb7ec6ab3847e5881280edd0f8171ec10769
cchsColin <71032299+cchsColin@users.noreply.github.com>
2020-10-16 13:21:38 -0400

    Add files via upload

---
+++ PhysicalObject/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Random;
+
+class Dice {
+    int sides;
+    Random rand = new Random();
+
+    public Dice(int sides){
+        this.sides = sides;
+    }
+
+    public String returnRoll(){
+        int roll = rand.nextInt(sides)+1;
+        String words = "You rolled a " + roll + "!";
+        return words;
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Dice six = new Dice(6);
+        System.out.println(six.returnRoll());
+    }
+}
